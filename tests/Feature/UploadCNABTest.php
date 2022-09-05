ec0ed29a9a7f7c0c35687cb0f108dec3e04a0a01
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-09-05 19:26:41 -0300

    criando teste para upload do aquivo

--- tests/Feature/UploadCNABTest.php
+++ tests/Feature/UploadCNABTest.php
@@ -15,11 +15,24 @@ class UploadCNABTest extends TestCase
     public function test_retorno_da_view_com_formulario()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
+        $response->assertViewIs('welcome');
         $response->assertSeeTextInOrder([
             'Formulário Upload',
             'Enviar'
         ]);
     }
+
+    /**
+     * @return void
+     */
+    public function test_envio_arquivo_formulario()
+    {
+        $response = $this->post('/');
+
+        $response->assertStatus(200);
+        $response->assertRedirect('/');
+        $response->assertViewHas('message', 'sucesso');
+    }
+
 }
