d49c771a8f126120a827a5a1292d4f4e22fa63bc
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-09-05 21:27:03 -0300

    atualizando html e css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,31 @@
 
     </head>
     <body class="antialiased">
+        @if(session('message'))
+            <div class="alert alert-message">
+                <span>{{ session('message') }}</span>
+            </div>
+        @endif
         <div class="container">
             <h1>Formulário Upload</h1>
             <form method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="row">
-                    <input type="file" name="file" class="upload">
+                    <input type="file" name="file" class="upload" accept=".txt">
                 </div>
                 <button type="submit" class="btn font-semibold">Enviar</button>
             </form>
+            <div class="errors">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li style="color: red">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
         </div>
     </body>
 </html>
