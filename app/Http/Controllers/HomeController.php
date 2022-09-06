792964a90e0cb210d28f83619d55356a292d3847
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-09-05 21:25:41 -0300

    finalizando função de upload de arquivo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\UploadFileRequest;
+use App\Http\Services\File\FileService;
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
+
+    private FileService $fileService;
+
+    public function __construct(FileService $fileService)
+    {
+        $this->fileService = $fileService;
+    }
 
     /**
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
@@ -12,6 +23,18 @@ class HomeController extends Controller
     public function index()
     {
         return view('welcome');
+    }
+
+    public function upload(UploadFileRequest $request)
+    {
+        $file = \request()->file('file');
+        $fileData = $this->fileService->readFile($file->path());
+
+        if ($this->fileService->store($fileData)){
+            return redirect('/', 201)
+                ->with('message', 'Success!');
+        }
+        return abort(501);
     }
 
 }
